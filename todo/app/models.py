2d1be2a819f35fdede4512185e3dcf0bc4519428
Keith Leonardo <kleonardo52@gmail.com>
2020-07-15 18:14:10 +1000

    Create new Item model for TODO list

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=1000)
+    description = models.TextField(default="")
+    status = models.IntegerField(default=0)
+
+    def __str__(self):
+        if self.status == 0:
+            status = "TODO"
+        elif self.status == 1:
+            status = "In-Progress"
+        return self.title + " " + self.status
